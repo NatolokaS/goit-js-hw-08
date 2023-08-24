987a52fef87ae280045eec8339b5f7d7e0b0fce2
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-08-25 01:33:07 +0300

    task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+listEl.style.listStyle = 'none';
+const galleryCardMarkup = renderList(galleryItems);
+
+function renderList(galleryItems) {
+  return galleryItems
+    .map(
+        (item) => `<li class="item gallery__item">
+      <a class="gallery__link" href="${item.original}" >
+  <img class="gallery__image" alt="${item.description}" src="${item.preview}" width="300" />
+  </a>
+  </li>`
+    )
+  .join("");
+};
+
+listEl.insertAdjacentHTML("beforeend", galleryCardMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  caption: true,
+  captionsData: "alt",
+  captionDelay: 250,
+});
