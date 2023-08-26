577bae1b4908decd2c38740530ef0897bd9245f3
NatolokaS <129997302+NatolokaS@users.noreply.github.com>
2023-08-27 01:27:27 +0300

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const item = "videoplayer-current-time";
+const onPlay = function(data) {
+    localStorage.setItem(item, data.seconds);
+};
+const throttleOnPlay = throttle(onPlay, 1000);
+player.on('timeupdate', throttleOnPlay);
+
+player.setCurrentTime(localStorage.getItem(item)).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
